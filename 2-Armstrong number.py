5154b8a13b3d11d609dfb96cff16f22b70dd3067
Bhaskar Patel <117108968+BinaryBhaskar@users.noreply.github.com>
2023-11-14 04:15:03 +0000

    Added Armstrong Number code

--- 2-Armstrong number.py
+++ 2-Armstrong number.py
@@ -1,1 +1,9 @@
+def armstrong(num):
+    exponent = len(str(num))
+    sum = 0
+    for n in str(num):
+        sum += int(n)**exponent
+    return num == sum
 
+print(1634,armstrong(1634))
+print(123,armstrong(123))
