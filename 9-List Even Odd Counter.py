759e34bfca54ac0cd7e84f3e100137cd809964bd
Bhaskar Patel <117108968+BinaryBhaskar@users.noreply.github.com>
2023-11-14 05:01:59 +0000

    Added Even Odd Counter code

--- 9-List Even Odd Counter.py
+++ 9-List Even Odd Counter.py
@@ -1,1 +1,12 @@
+def counter(list):
+    counteven = countodd = 0
+    for i in list:
+        if i%2 == 0:
+            counteven += 1
+        else:
+            countodd += 1
+    print(f"Even Numbers:{counteven}\nOdd Numbers:{countodd}")
 
+
+list = [13,74,34,70,38,85,57]
+counter(list)
