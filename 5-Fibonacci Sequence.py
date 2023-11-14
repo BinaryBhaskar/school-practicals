d28a8db174640b956aef5afa015509347043c878
Bhaskar Patel <117108968+BinaryBhaskar@users.noreply.github.com>
2023-11-14 04:37:33 +0000

    Added Fibonacci Sequence code

--- 5-Fibonacci Sequence.py
+++ 5-Fibonacci Sequence.py
@@ -1,1 +1,12 @@
+def fibonacci(n):
+    n1, n2 = 0, 1
+    next = n2  
+    count = 1
+    while count <= n:
+        print(next, end=" ")
+        count += 1
+        n1, n2 = n2, next
+        next = n1 + n2
+    print()
 
+fibonacci(7)
