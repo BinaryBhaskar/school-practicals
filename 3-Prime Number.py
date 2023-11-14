b9ece138f90fbbfd1ad7df2730e3f51ab8304fb4
Bhaskar Patel <117108968+BinaryBhaskar@users.noreply.github.com>
2023-11-14 04:21:58 +0000

    Added Prime Number code

--- 3-Prime Number.py
+++ 3-Prime Number.py
@@ -1,1 +1,11 @@
+def isprime(n):
+    if n <=1:
+        return False
+    for i in range(2,n):
+        if n%i == 0:
+            return False
+    return True
 
+
+print(13, isprime(13))
+print(16, isprime(16))
